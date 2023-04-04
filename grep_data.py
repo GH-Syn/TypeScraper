8f4252ff434948f629a52cf983845cca8b03ec93
Joshua Rose <joshuarose099@gmail.com>
2023-04-04 10:15:09 +1000

    feat: added ensure ascii and sort keys to json

--- grep_data.py
+++ grep_data.py
@@ -114,4 +114,4 @@ for profile in profiles:
     }
 
 
-json.dump(profiles_as_hashmap, open(os.path.join("src", "profiles.json"), "w"))
+json.dump(profiles_as_hashmap, open(os.path.join("src", "profiles.json"), "w"), sort_keys=True, ensure_ascii=True)
